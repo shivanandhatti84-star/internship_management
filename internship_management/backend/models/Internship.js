82b434a55fab64635f3479bc491153f14583114e
shivanandhatti84-star <shivanandhatti84@gmail.com>
2026-05-31 21:39:47 +0530

    Update Internship.js

--- internship_management/backend/models/Internship.js
+++ internship_management/backend/models/Internship.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const internshipSchema = new mongoose.Schema({
   company: { type: String, required: true },
-  location: { type: String, required: true },
+  location: { type: String },
   duration: { type: String, required: true },
   startDate: { type: String, required: true },
   stipend: { type: Number, required: true },
